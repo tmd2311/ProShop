706298e81d8d0148feaf0344a7130a048560958b
tmd2311 <dung.kayc@gmail.com>
2025-09-22 00:49:06 +0700

    feat : thêm chức năng quản lí user

--- auth-service/src/main/java/com/proshop/auth/repository/UserRepository.java
+++ auth-service/src/main/java/com/proshop/auth/repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.proshop.auth.repository;
 
 import com.proshop.auth.entity.UserEntity;
 import com.proshop.auth.entity.UserRoleEntity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -23,4 +28,13 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
   @Query("select ur.roleEntity.name from UserRoleEntity ur where ur.userEntity.id = :userId ")
   List<String> getRoleNamesByUserId(@Param("userId") Long userId);
+
+  boolean existsByAccount(
+      @NotBlank(message = "Account is required") @Size(min = 4, max = 50, message = "Account must be between 4 and 50 characters") String account);
+
+  boolean existsByEmail(
+      @NotBlank(message = "Email is required") @Email(message = "Invalid email format") String email);
+
+  Page<UserEntity> findAllByDeletedFalse(Pageable pageable);
+
 }
